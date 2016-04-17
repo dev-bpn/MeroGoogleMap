f5cab557e3785fe93d3c62530b2eedbb0313b6fb
dipesh.dhakal <dboy.gilchrist@gmail.com>
2016-04-17 12:14:37 +0545

    Second Commit

--- app/src/main/java/com/bipin/android/googlemap/MapsActivity.java
+++ app/src/main/java/com/bipin/android/googlemap/MapsActivity.java
@@ -48,7 +48,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
          mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
          mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
-        
+
 
         // Change the Map Type
         /**
